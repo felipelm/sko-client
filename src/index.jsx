885367173274fd70f9d047e0885d8ac9027dad4f
“felipelm” <felipelm123@gmail.com>
2017-02-05 14:35:58 -0200

    adding middleware to send state to node server via socket

--- src/index.jsx
+++ src/index.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {createStore} from 'redux';
+import {createStore, applyMiddleware} from 'redux';
 import {Provider} from 'react-redux';
 import reducer from './reducer';
 import {Router, Route, hashHistory} from 'react-router';
@@ -9,14 +9,18 @@ import {VotingContainer} from './components/Voting';
 import {ResultsContainer} from './components/Results';
 import io from 'socket.io-client';
 import {setState} from './action_creators';
-
-const store = createStore(reducer);
-
+import remoteActionMiddleware from './remote_action_middleware';
 
 const socket = io(`${location.protocol}//${location.hostname}:8090`);
 socket.on('state', state =>
   store.dispatch(setState(state))
 );
+
+const createStoreWithMiddleware = applyMiddleware(
+  remoteActionMiddleware(socket)
+)(createStore)
+
+const store = createStoreWithMiddleware(reducer);
 
 const routes = <Route component={App}>
   <Route path="/results" component={ResultsContainer} />
