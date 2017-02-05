9dd76b57525cd793ecb59d4b4e8d7e92c8484b10
“felipelm” <felipelm123@gmail.com>
2017-02-05 11:44:04 -0200

    wrapping Voting component to Redux store

--- src/components/Voting.jsx
+++ src/components/Voting.jsx
@@ -2,8 +2,9 @@ import React from 'react';
 import shallowCompare from 'react-addons-shallow-compare';
 import Winner from './Winner';
 import Vote from './Vote';
+import {connect} from 'react-redux';
 
-export default React.createClass({
+export const Voting = React.createClass({
   shouldComponentUpdate: function(nextProps, nextState) {
     return shallowCompare(this, nextProps, nextState);
   },
@@ -15,3 +16,12 @@ export default React.createClass({
       </div>;
   }
 });
+
+function mapStateToProps(state){
+  return {
+    pair: state.getIn(['vote', 'pair']),
+    winner: state.get('winner')
+  };
+}
+
+export const VotingContainer = connect(mapStateToProps)(Voting);
