ac6bd4c89213afc852d78a2cb9c95accec45e59e
“felipelm” <felipelm123@gmail.com>
2017-02-05 11:52:32 -0200

    removing app data placeholders

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,14 +1,7 @@
 import React from 'react';
-import {List, Map} from 'immutable';
-
-const pair = List.of('McDonalds', 'Bobs');
-const score = Map({'McDonalds': 3, 'Bobs': 2});
 
 export default React.createClass({
   render: function(){
-    return React.cloneElement(this.props.children, {
-      pair: pair,
-      score: score
-    });
+    return this.props.children;
   }
 });
