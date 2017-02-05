02798e23e4abf2862fe9d608f4b6a7bc80faa9ee
“felipelm” <felipelm123@gmail.com>
2017-02-05 09:44:15 -0200

    adding results to Results component

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,10 +1,14 @@
 import React from 'react';
-import {List} from 'immutable';
+import {List, Map} from 'immutable';
 
 const pair = List.of('McDonalds', 'Bobs');
+const score = Map({'McDonalds': 3, 'Bobs': 2});
 
 export default React.createClass({
   render: function(){
-    return React.cloneElement(this.props.children, {pair: pair});
+    return React.cloneElement(this.props.children, {
+      pair: pair,
+      score: score
+    });
   }
 });
--- src/components/Results.jsx
+++ src/components/Results.jsx
@@ -5,7 +5,25 @@ export default React.createClass({
   shouldComponentUpdate: function(nextProps, nextState) {
     return shallowCompare(this, nextProps, nextState);
   },
+  getPair: function(){
+    return this.props.pair || [];
+  },
+  getVotes: function(entry){
+    if(this.props.score && this.props.score.has(entry)){
+      return this.props.score.get(entry);
+    }
+    return 0;
+  },
   render: function(){
-    return <div>Resultados!</div>
+    return <div className="results">
+      {this.getPair().map(entry =>
+        <div key={entry} className="entry">
+          <h1>{entry}</h1>
+          <div className="voteCount">
+            {this.getVotes(entry)}
+          </div>
+        </div>
+      )}
+      </div>;
   }
 });
